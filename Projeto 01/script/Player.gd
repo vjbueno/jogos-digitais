f20537cee04770de21221161fd0df250926bb702
Bueno Vital <buenovj@hotmail.com>
2021-09-20 21:54:58 -0300

    Atribuindo movimentos ao sprite

--- Projeto 01/script/Player.gd
+++ Projeto 01/script/Player.gd
@@ -1,10 +1,19 @@
 extends KinematicBody2D
 
-const MOVE_SPEED = 15
+
+const MOVE_SPEED = 25 #velocidade de movimento
 var move_dir = 0
 
+const JUMP_FORCE = 1100 #força do pulo
+const GRAVITY = 50 #gravidade
+const MAX_FALL_SPEED = 500 #velocidade máxima de queda do personagem após o pulo
+
+var y_velo = 0 #velocidade minima
 
 func _physics_process(delta): #FPS
+	
+	var grounded = is_on_floor();
+	var no_chao = $RayD.is_colliding() or $RayE.is_colliding()
 	
 	if Input.is_action_pressed("right"):
 		
@@ -21,4 +30,16 @@ func _physics_process(delta): #FPS
 		$ninjaJubileu.animation ="Respirando"
 		move_dir = 0
 		
-	move_and_slide(Vector2(move_dir * MOVE_SPEED, 0), Vector2(0,-1))
+	if grounded and Input.is_action_just_pressed("jump"):
+		y_velo = -JUMP_FORCE
+		$ninjaJubileu.animation = "jump"
+		
+	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0,-1))
+	
+	y_velo += GRAVITY
+	
+	if grounded and y_velo >= 0 :
+		y_velo = 5
+		
+	if y_velo > MAX_FALL_SPEED:
+		y_velo = MAX_FALL_SPEED
